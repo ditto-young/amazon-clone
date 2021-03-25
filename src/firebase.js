24e100da7e0e01f156a1ae28658d2b2a45d60437
gpdud0243 <gpdud0243@naver.com>
2021-03-25 22:03:52 +0900

    amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9ih323L1fz25OgZXiUzs6CnAVAEL20L8",
+  authDomain: "challenge-896df.firebaseapp.com",
+  projectId: "challenge-896df",
+  storageBucket: "challenge-896df.appspot.com",
+  messagingSenderId: "609617652212",
+  appId: "1:609617652212:web:a156d6d320fc7965def1a1",
+  measurementId: "G-X9WNJMEDXG"
+};
