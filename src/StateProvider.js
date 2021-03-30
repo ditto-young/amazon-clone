9d5525de92ac7916a22e35ad1b2baeb5e9a56246
gpdud0243 <gpdud0243@naver.com>
2021-03-30 22:37:31 +0900

    2:57:21

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -5,11 +5,11 @@ import React, {createContext, useContext, useReducer} from 'react';
 export const StateContext = createContext();
 
 //Wrap our app and provier the Data layer
-export const StateProvider = ({ reducer, initialState, children}) => {
+export const StateProvider = ({ reducer, initialState, children}) => (
     <StateContext.Provider value = {useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-};
+);
 
 //Pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
             <CurrencyFormat
-                renderText={(value) => {
+                renderText={(value) => (
                     <>
                         <p>
-                            
-                            Subtotal (0 items): <strong>0</strong>
+                            Subtotal ({basket.length} items): <strong>{value}</strong>
                         </p>
                         <small className="subtotal_gift">
                             <input type="checkbox"/> this order contains a gift
                         </small>
                     </>
-                }}
+                )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
             />
-            <button> Proceed to Checkout</button>
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
